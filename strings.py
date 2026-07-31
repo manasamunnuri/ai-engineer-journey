c30eb31610d330ad06acacd64700bbb804423607
manasamunnuri <munnurmanasa28@gmail.com>
2026-07-31 06:16:33 +0530

    Update strings.py
    employee profile generator using strings and slicing

--- strings.py
+++ strings.py
@@ -1,3 +1,4 @@
+#Employee profile generator#
 first_name='Manasa'
 last_name='Munnuri'
 full_name=first_name +' '+ last_name
@@ -16,9 +17,9 @@ salary = 100000
 employee_card= f'Employee: {full_name} | Age:{employee_age} | Position: {position} | Salary:{salary}'
 print(employee_card)
 employee_code = 'AI-2026-JD-001'
-department = employee_code[0:2]
+department = employee_code[0:3]
 print(department)
-year_code = employee_code[3:7]
+year_code = employee_code[4:8]
 print(year_code)
 initials = employee_code[8:10]
 print(initials)
